47b60bc11d0e20f70663a83b800c46f78cf38238
Stuart Dum <stuart.dum@gmail.com>
2021-06-23 16:56:43 -0400

    Create script for setting labels

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# Default files
+# Defaults
 
 [Supported file types](https://docs.github.com/en/communities/setting-up-your-project-for-healthy-contributions/creating-a-default-community-health-file#supported-file-types)
 
-## Default labels
+## .env
 
-| Name               | Description                                                  | Color   |
-| ------------------ | ------------------------------------------------------------ | ------- |
-| bug                | unintended behavior that can be related to features          | #EF4444 |
-| chore              | stories that are necessary but provide no direct, obvious value to the customer | #D1D5DB |
-| feature (0 points) | stories that provide verifiable business value to the team’s customer | #ECFDF5 |
-| feature (1 point)  | stories that provide verifiable business value to the team’s customer | #D1FAE5 |
-| feature (2 points) | stories that provide verifiable business value to the team’s customer | #A7F3D0 |
-| feature (3 points) | stories that provide verifiable business value to the team’s customer | #6EE7B7 |
-| feature (5 points) | consider creating a milestone for this with more fine-grained pieces | #10B981 |
-| feature (8 points) | consider creating a milestone for this with more fine-grained pieces | #065F46 |
+```txt
+ORG=classicalacademicpress
+TOKEN={SOME_GITHUB_TOKEN}
+```
+
+## Repo labels
+
+1. Add labels to `default-labels.json`
+2. Run `deno run --allow-read --allow-net set-labels.ts`
---
+++ set-labels.ts
@@ -0,0 +1,117 @@
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+// Config
+
+const { ORG, TOKEN } = config();
+
+// Types
+
+type Repo = { name: string };
+
+type Label = {
+  id?: number;
+  node_id?: string;
+  url?: string;
+  name: string;
+  description: string;
+  color: string;
+  default?: boolean;
+};
+
+// API calls
+
+const getRepos = (): Promise<Response> =>
+  fetch(
+    `https://api.github.com/orgs/${ORG}/repos?access_token=${TOKEN}`,
+    {
+      headers: {
+        "Accept": "application/vnd.github.v3+json",
+      },
+    },
+  );
+
+const getLabels = (repo: string): Promise<Response> =>
+  fetch(
+    `https://api.github.com/repos/${ORG}/${repo}/labels?access_token=${TOKEN}`,
+    {
+      headers: {
+        "Accept": "application/vnd.github.v3+json",
+      },
+    },
+  );
+
+const createLabel = (
+  repo: string,
+  label: Label,
+): Promise<
+  Response
+> => (console.info(`Creating label ${label.name} on ${repo}...`),
+  fetch(
+    `https://api.github.com/repos/${ORG}/${repo}/labels?access_token=${TOKEN}`,
+    {
+      headers: {
+        "Accept": "application/vnd.github.v3+json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        name: label.name,
+        color: label.color,
+        description: label.description,
+      }),
+    },
+  ));
+
+const deleteLabel = (
+  label: Label,
+): Promise<Response> => (console.info(`Deleting label ${label.name}...`),
+  fetch(
+    `${label.url}?access_token=${TOKEN}`,
+    {
+      headers: {
+        "Accept": "application/vnd.github.v3+json",
+      },
+      method: "DELETE",
+    },
+  ));
+
+// Helpers
+
+const repoToJson = (response: Response): Promise<Repo[]> => response.json();
+const labelToJson = (response: Response): Promise<Label[]> => response.json();
+const repoName = (repo: Repo): string => repo.name;
+
+const addDefaultLabels = (defaultLabels: Label[]) =>
+  (repoName: string) =>
+    defaultLabels.map((defaultLabel: Label): Promise<Response> =>
+      createLabel(repoName, defaultLabel)
+    );
+
+const isCustomLabel = (defaultLabels: Label[]) =>
+  (label: Label) =>
+    defaultLabels.find((defaultLabel: Label): boolean =>
+      defaultLabel.name === label.name && defaultLabel.color === label.color &&
+      defaultLabel.description === label.description
+    ) === undefined;
+
+// Program
+
+const decoder: TextDecoder = new TextDecoder("utf-8");
+const data: BufferSource = Deno.readFileSync("default-labels.json");
+const defaultLabels: Label[] = JSON.parse(decoder.decode(data));
+
+const updateLabels = (repoNames: string[]): Promise<Response[]> =>
+  Promise.all(repoNames.map(getLabels)).then((
+    responses: Response[],
+  ): Promise<Label[][]> => Promise.all(responses.map(labelToJson))).then((
+    json: Label[][],
+  ): Promise<Response[]> =>
+    Promise.all(
+      json.flat().filter(isCustomLabel(defaultLabels)).map(deleteLabel),
+    )
+  ).then((_responses: Response[]): Promise<Response[]> =>
+    Promise.all(repoNames.map(addDefaultLabels(defaultLabels)).flat())
+  );
+
+getRepos().then(repoToJson).then(
+  (json: Repo[]): string[] => json.map(repoName),
+).then(updateLabels).then((_) => console.info("Done!"));
